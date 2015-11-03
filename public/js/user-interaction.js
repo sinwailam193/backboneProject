794425e75dc02ccdd2a1d71df59b4ae55422a6dc
Sin Wai Lam <sinwailam193@hotmail.com>
2015-11-02 20:09:40 -0800

    added session

--- public/js/user-interaction.js
+++ public/js/user-interaction.js
@@ -6,6 +6,18 @@ socket.on('change', function(){
   blogsView.model.fetch();
 })
 
+$.get( "/sess", function(data) {
+  if(data === "none"){
+    $('.usernew').hide();
+  }
+  else{
+    $('.username-input').val(data);
+    $('.username-input').hide();
+    $('.username').html(data);
+    $('.username').show();
+  }
+});
+
 $(function(){
   $('.add-blog').on('click', function(){
     if($('.author-input').val() && $('.title-input').val() && $('.url-input').val()){
@@ -24,19 +36,31 @@ $(function(){
   });
 
   $('.send-text').on('click', function(){
-    var user = $('.username-input').val();
-    var message = new Message({
-      username: user,
-      message: $('.text-input').val()
-    });
-    if($('.username-input').val()){
-      $('.username-input').hide();
-      $('.username').html(user);
-      $('.username').show();
+    if($('.text-input').val()){
+      var user = $('.username-input').val();
+      var message = new Message({
+        username: user,
+        message: $('.text-input').val()
+      });
+      if($('.username-input').val()){
+        $('.username-input').hide();
+        $('.username').html(user);
+        $('.username').show();
+        $('.usernew').show();
+      }
+      messages.add(message);
+      $('.text-input').val('')
+      message.save(null);
+      socket.emit('chat')//telling socket there is something changing
     }
-    messages.add(message);
-    $('.text-input').val('')
-    message.save(null);
-    socket.emit('chat')//telling socket there is something changing
+  })
+
+  $('.usernew').on('click', function(){
+    $.post( "/close", {});
+    $('.username-input').val('');
+    $('.username-input').show();
+    $('.username').html('');
+    $('.username').hide();
+    $('.usernew').hide();
   })
 })
